fddb816b7896f7e06e9f9a7206b406d17da2e8c3
unknown <liuhaiming>
2020-01-08 09:27:45 +0800

    初始化

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <v-input></v-input>
+  </div>
+</template>
+
+<script>
+import vInput from '../components/base/input'
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+  components: {
+    vInput
+  }
+}
+</script>
