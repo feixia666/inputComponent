6ce847b0dea34e0c57d1e5960af3c756a13a9058
unknown <liuhaiming>
2020-01-08 18:12:51 +0800

    1.8晚上

--- src/components/base/input2.vue
+++ src/components/base/input2.vue
@@ -1,6 +1,38 @@
 <template>
-  <div>
-    <input type="text" :value="value" @input="update" />
+  <div class="main">
+    <template v-if="type == 'text'">
+      <input
+        class="input-area"
+        type="text"
+        v-model="newValue"
+        :placeholder="placeholder"
+        :disabled="disabled"
+      />
+      <span
+        class="clean"
+        @click="cleanAll"
+        v-if="clearable"
+        v-show="newValue != ''"
+        >x</span
+      >
+    </template>
+    <template v-if="type == 'textarea'">
+      <textarea
+        class="textarea"
+        :cols="areasize.cols"
+        :rows="areasize.rows"
+        v-model="newValue"
+        :placeholder="placeholder"
+        :disabled="disabled"
+      ></textarea>
+      <span
+        class="clean"
+        @click="cleanAll"
+        v-if="clearable"
+        v-show="newValue != ''"
+        >x</span
+      >
+    </template>
   </div>
 </template>
 
@@ -10,18 +42,69 @@ export default {
     value: {
       type: String,
       value: ''
+    },
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: null
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    clearable: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    newValue: {
+      get() {
+        return this.value
+      },
+      set: function(value) {
+        return this.$emit('input', value)
+      }
     }
   },
   data() {
     return {
-      show: ''
+      areasize: {
+        cols: 20,
+        rows: 2
+      }
     }
   },
-
   methods: {
-    update(val) {
-      this.$emit('input', val)
+    cleanAll() {
+      this.newValue = ''
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.main {
+  position: relative;
+  .clean {
+    display: inline-block;
+    font-size: 12px;
+    width: 12px;
+    height: 12px;
+    line-height: 12px;
+    border: 1px solid #b4bccc;
+    border-radius: 50%;
+    cursor: pointer;
+    position: absolute;
+    left: 500px;
+    top: 50%;
+    margin-top: -6px;
+    color: #b4bccc;
+  }
+  .textarea {
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,31 +1,34 @@
 <template>
   <div class="about">
     <h1>这是input组件</h1>
-    <v-input v-model="msg" placeholder="ashhh"></v-input>
-    <v-input v-model="msg1" :disabled="true"></v-input>
-    <dy-input v-model="msg2"></dy-input>
+    <v-input
+      v-model="msg"
+      type="textarea"
+      clearable
+      placeholder="请输入"
+    ></v-input>
+    <v-input v-model="msg3" placeholder="请输入" clearable></v-input>
+
     <!-- <input type="text" v-model="msg" /> -->
     <div>{{ msg }}</div>
-    <div>{{ msg1 }}</div>
-    <div>{{ msg2 }}</div>
+    <div>{{ msg3 }}</div>
   </div>
 </template>
 
 <script>
-import vInput from '../components/base/input'
-import dyInput from '../components/base/input2'
+import vInput from '../components/base/input2'
 export default {
   data() {
     return {
       msg: '',
       msg1: '',
-      msg2: ''
+      msg2: '',
+      msg3: ''
     }
   },
   methods: {},
   components: {
-    vInput,
-    dyInput
+    vInput
   }
 }
 </script>
